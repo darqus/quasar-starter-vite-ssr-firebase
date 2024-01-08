7e7c9794ec4fc7f88945959e4dff45d888f45455
darqus <voladores@inbox.ru>
2024-01-08 20:17:22 +0300

    fix design LoginPage

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -6,86 +6,132 @@
       @submit.prevent="storeAuth.onLogin"
     >
       <q-card style="min-width: 300px">
-        <q-card-section class="bg-primary text-white">
+        <!-- <q-card-section class="bg-primary text-white">
           <div class="text-h6">
             <div>Вход</div>
           </div>
-        </q-card-section>
+        </q-card-section> -->
 
         <q-card-section>
-          <q-input
-            v-for="field in storeAuth.formsAuth[AUTH_TYPE.LOGIN_EMAIL]"
-            :key="field.id"
-            v-model="field.model"
-            :debounce="field.debounce"
-            :label="field.required ? `${field.label}${INPUT_REQUIRED}` : field.label"
-            :mask="field.mask"
-            :name="field.name"
-            :required="field.required"
-            :rules="field.rule"
-            :type="field.inputType === INPUT_TYPE.PASSWORD ? storeAuth.currentInputType : field.inputType"
-            class="q-mb-sm"
-            lazy-rules
-            outlined
-          >
-            <template #prepend>
-              <q-icon
-                :name="field.iconPrepend"
-                class="cursor-pointer"
-              />
-            </template>
-            <template #append>
-              <q-icon
-                v-if="field.inputType === INPUT_TYPE.PASSWORD"
-                :name="storeAuth.iconPassword"
-                class="cursor-pointer"
-                @click="storeAuth.togglePasswordVisible"
-              />
-            </template>
-          </q-input>
+          <div class="row">
+            <div class="col-0 col-sm-5 bg-primary xs-hide">
+              <div class="row full-width q-px-xl q-pb-xl full-height flex flex-center">
+                <div class="">
+                  <div
+                    class="text-h4 text-white"
+                    style="min-width: 220px"
+                  >
+                    Добро пожаловать
+                  </div>
+                  <div class="text-white q-my-sm text-subtitle1">
+                    Для начала работы войдите в ваш аккаунт!
+                  </div>
+                </div>
+              </div>
+            </div>
 
-          <div class="row -justify-between">
-            <q-btn
-              :disable="storeAuth.loading"
-              :type="BUTTON_TYPE.RESET"
-              color="primary"
-              label="Очистить"
-              no-caps
-              outline
-            />
-            <q-btn
-              :disable="storeAuth.disabledSubmitButton"
-              :loading="storeAuth.loading"
-              :type="BUTTON_TYPE.SUBMIT"
-              color="primary"
-              label="Войти"
-              style="width: 200px; margin-left: 15px;"
-              no-caps
-            />
-          </div>
+            <div class="col-12 col-sm-7">
+              <div class="row q-ml-sm q-mt-sm sm-and-up-hide">
+                <div class="col-12 text-subtitle1">
+                  <router-link
+                    class="text-primary"
+                    style="text-decoration: none"
+                    to="/"
+                  >
+                    На Главную
+                  </router-link>
+                </div>
+              </div>
+              <div class="row q-pa-sm-sm q-pa-md">
+                <div class="col-12">
+                  <q-card-section>
+                    <div class="q-mb-xl">
+                      <div class="flex justify-center">
+                        <div class="text-h4 q-my-none text-weight-bold text-primary">
+                          Вход
+                        </div>
+                      </div>
+                    </div>
 
-          <div class="row q-mt-md">
-            <q-btn
-              class="full-width"
-              color="primary"
-              size="12px"
-              no-caps
-              outline
-              @click="$router.push(ROUTE_TYPE.FORGOT)"
-            >
-              Забыли пароль?
-            </q-btn>
-          </div>
+                    <q-input
+                      v-for="field in storeAuth.formsAuth[AUTH_TYPE.LOGIN_EMAIL]"
+                      :key="field.id"
+                      v-model="field.model"
+                      :debounce="field.debounce"
+                      :label="field.required ? `${field.label}${INPUT_REQUIRED}` : field.label"
+                      :mask="field.mask"
+                      :name="field.name"
+                      :required="field.required"
+                      :rules="field.rule"
+                      :type="field.inputType === INPUT_TYPE.PASSWORD ? storeAuth.currentInputType : field.inputType"
+                      class="q-mb-sm"
+                      lazy-rules
+                      outlined
+                    >
+                      <template #prepend>
+                        <q-icon
+                          :name="field.iconPrepend"
+                          class="cursor-pointer"
+                        />
+                      </template>
+                      <template #append>
+                        <q-icon
+                          v-if="field.inputType === INPUT_TYPE.PASSWORD"
+                          :name="storeAuth.iconPassword"
+                          class="cursor-pointer"
+                          @click="storeAuth.togglePasswordVisible"
+                        />
+                      </template>
+                    </q-input>
 
-          <div class="q-mt-lg">
-            <div class="q-mt-sm">
-              Нет аккаунта?
-              <router-link
-                :to="ROUTE_TYPE.REGISTER"
-                class="text-primary"
-              >
-                Зарегистрироваться
-              </router-link>
+                    <div class="row -justify-between">
+                      <q-btn
+                        :disable="storeAuth.loading"
+                        :type="BUTTON_TYPE.RESET"
+                        color="primary"
+                        label="Очистить"
+                        no-caps
+                        outline
+                      />
+                      <q-btn
+                        :disable="storeAuth.disabledSubmitButton"
+                        :loading="storeAuth.loading"
+                        :type="BUTTON_TYPE.SUBMIT"
+                        class="col-grow"
+                        color="primary"
+                        label="Войти"
+                        style="margin-left: 15px;"
+                        no-caps
+                      />
+                    </div>
+
+                    <div class="row q-mt-md">
+                      <q-btn
+                        class="full-width"
+                        color="primary"
+                        size="12px"
+                        no-caps
+                        outline
+                        @click="$router.push(ROUTE_TYPE.FORGOT)"
+                      >
+                        Забыли пароль?
+                      </q-btn>
+                    </div>
+
+                    <div class="q-mt-lg">
+                      <div class="q-mt-sm">
+                        Нет аккаунта?
+                        <router-link
+                          :to="ROUTE_TYPE.REGISTER"
+                          class="text-primary"
+                        >
+                          Зарегистрироваться
+                        </router-link>
+                      </div>
+                    </div>
+                  </q-card-section>
+                </div>
+              </div>
             </div>
           </div>
         </q-card-section>
