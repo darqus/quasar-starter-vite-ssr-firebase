b1aeae06bc21eb4b38828cb511ee87d0b6063143
darqus <voladores@inbox.ru>
2024-01-09 00:11:25 +0300

    fix design account page

--- src/pages/account/AccountPage.vue
+++ src/pages/account/AccountPage.vue
@@ -1,21 +1,56 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <q-card style="min-width: 300px">
-      <q-card-section class="bg-primary text-white">
-        <div class="text-h6">
-          Профиль
-        </div>
-      </q-card-section>
+    <q-card
+      class="shadow-up-1"
+      style="min-width: 300px; max-width: 700px;"
+    >
+      <div class="row">
+        <q-card class="col-sm-5 bg-blue-8 xs-hide text-right">
+          <div class="bg-blue-6 text-white q-pa-md">
+            <div
+              class="text-h6 text-white"
+              style="min-width: 220px"
+            >
+              Добро пожаловать
+            </div>
+          </div>
+          <div class="bg-blue-5 text-white q-pa-md">
+            <div class="text-white q-my-sm text-subtitle1">
+              Это данные вашего аккаунта!
+            </div>
+          </div>
+        </q-card>
 
-      <q-card-section>
-        <q-input
-          :model-value="storeAuth.currentUser?.email"
-          label="Email"
-          dense
-          disable
-          outlined
-        />
-      </q-card-section>
+        <div class="col-sm-7 shadow-1">
+          <div class="q-pa-lg">
+            <div class="row">
+              <div class="col-12 text-subtitle1">
+                <router-link
+                  class="text-primary"
+                  to="/"
+                >
+                  На Главную
+                </router-link>
+              </div>
+              <div class="col-12">
+                <div class="flex justify-center">
+                  <div class="text-h5 q-my-lg text-weight-bold text-primary">
+                    Профиль
+                  </div>
+                </div>
+
+                <q-input
+                  :model-value="storeAuth.currentUser?.email"
+                  label="Email"
+                  dense
+                  disable
+                  outlined
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </q-card>
   </q-page>
 </template>
