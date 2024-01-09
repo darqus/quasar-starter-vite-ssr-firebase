fb95b71cd5da80b036acff3f4de77295b208cd23
darqus <voladores@inbox.ru>
2024-01-09 12:16:37 +0300

    fix design new employee

--- src/pages/employee/NewEmployeePage.vue
+++ src/pages/employee/NewEmployeePage.vue
@@ -5,63 +5,102 @@
       @reset="storeNewEmployee.onReset"
       @submit.prevent="storeNewEmployee.add"
     >
-      <q-card style="min-width: 300px">
-        <q-card-section class="bg-primary text-white">
-          <div class="text-h6">
-            Новый сотрудник
-          </div>
-        </q-card-section>
+      <q-card
+        class="shadow-up-1"
+        style="min-width: 300px; max-width: 700px;"
+      >
+        <div class="row">
+          <q-card class="col-sm-5 bg-blue-8 xs-hide text-right">
+            <div class="bg-blue-6 text-white q-pa-md">
+              <div
+                class="text-h6 text-white"
+                style="min-width: 220px"
+              >
+                Добро пожаловать
+              </div>
+            </div>
+            <div class="bg-blue-5 text-white q-pa-md">
+              <div class="text-white q-my-sm text-subtitle1">
+                Создайте нового сотрудника!
+              </div>
+            </div>
+          </q-card>
 
-        <q-card-section>
-          <template
-            v-for="field in storeNewEmployee.formEmployee"
-            :key="field.id"
-          >
-            <template v-if="field.formFieldType === FORM_FIELD_TYPE.INPUT">
-              <q-input
-                v-model="field.model"
-                :debounce="field.debounce"
-                :label="field.required ? `${field.label}${INPUT_REQUIRED}` : field.label"
-                :mask="field.mask"
-                :name="field.name"
-                :required="field.required"
-                :rules="field.rule"
-                :type="field.inputType"
-                outlined
-              />
-            </template>
-            <template v-if="field.formFieldType === FORM_FIELD_TYPE.SELECT">
-              <q-select
-                v-model="field.model"
-                :label="field.required ? `${field.label}${INPUT_REQUIRED}` : field.label"
-                :options="field.options"
-                :required="field.required"
-                :rules="field.rule"
-                lazy-rules
-                outlined
-              />
-            </template>
-          </template>
+          <div class="col-sm-7 shadow-1">
+            <div class="q-pa-lg">
+              <div class="row">
+                <div class="col-12 text-subtitle1">
+                  <router-link
+                    class="text-primary"
+                    to="/"
+                  >
+                    На Главную
+                  </router-link>
+                </div>
+                <div class="col-12">
+                  <div class="flex justify-center">
+                    <div class="text-h5 q-my-lg text-weight-bold text-primary">
+                      Новый сотрудник
+                    </div>
+                  </div>
 
-          <div class="row justify-between q-mt-md">
-            <q-btn
-              :disable="storeNewEmployee.loading"
-              :type="BUTTON_TYPE.RESET"
-              color="primary"
-              label="Сбросить"
-              no-caps
-              outline
-            />
-            <q-btn
-              :disable="storeNewEmployee.disabledSubmitButton"
-              :loading="storeNewEmployee.loading"
-              :type="BUTTON_TYPE.SUBMIT"
-              color="primary"
-              label="Добавить"
-              no-caps
-            />
+                  <template
+                    v-for="field in storeNewEmployee.formEmployee"
+                    :key="field.id"
+                  >
+                    <template v-if="field.formFieldType === FORM_FIELD_TYPE.INPUT">
+                      <q-input
+                        v-model="field.model"
+                        :debounce="field.debounce"
+                        :label="field.required ? `${field.label}${INPUT_REQUIRED}` : field.label"
+                        :mask="field.mask"
+                        :name="field.name"
+                        :required="field.required"
+                        :rules="field.rule"
+                        :type="field.inputType"
+                        outlined
+                      />
+                    </template>
+                    <template v-if="field.formFieldType === FORM_FIELD_TYPE.SELECT">
+                      <q-select
+                        v-model="field.model"
+                        :label="field.required ? `${field.label}${INPUT_REQUIRED}` : field.label"
+                        :options="field.options"
+                        :required="field.required"
+                        :rules="field.rule"
+                        lazy-rules
+                        outlined
+                      />
+                    </template>
+                  </template>
+
+                  <div class="row justify-between q-mt-md">
+                    <q-btn
+                      :disable="storeNewEmployee.loading"
+                      :type="BUTTON_TYPE.RESET"
+                      color="primary"
+                      label="Сбросить"
+                      no-caps
+                      outline
+                      rounded
+                    />
+                    <q-btn
+                      :disable="storeNewEmployee.disabledSubmitButton"
+                      :loading="storeNewEmployee.loading"
+                      :type="BUTTON_TYPE.SUBMIT"
+                      class="col-grow"
+                      color="primary"
+                      label="Добавить"
+                      style="margin-left: 15px;"
+                      no-caps
+                      rounded
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </q-card-section>
+        </div>
       </q-card>
     </q-form>
   </q-page>
