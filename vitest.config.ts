3da2c671406ec9e1457e8706a58f01be658ee5b1
darqus <voladores@inbox.ru>
2024-04-03 03:49:16 +0300

    fix test config

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { quasar, transformAssetUrls, } from '@quasar/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    environment: 'happy-dom',
+    setupFiles: 'test/vitest/setup-file.ts',
+    include: [
+      'test/vitest/__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    ],
+  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls, },
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.scss',
+    }),
+    tsconfigPaths(),
+  ],
+})
